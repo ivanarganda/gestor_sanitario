45d9f7fa6027c8750263084ea67151479e4346fa
ivanarganda <ivanartista96@gmail.com>
2024-05-17 18:47:35 +0200

    Añadida la funcion de activar o desactivar usuario mediante los checkboxes en la tabla de usuarios en la parte de acciones

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,28 @@
 <?php
-
+// web.php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MailController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('main');
 });
 
+Route::middleware(['check.login'])->group(function () {
+    Route::get('/users/{user_name?}', [UserController::class, 'getUsers'])->name('users');
+    Route::post('/users/update/{id}', [UserController::class, 'update']);
+    Route::post('/users/create', [UserController::class, 'register']);
+    Route::get('/users/delete/{id}', [UserController::class, 'delete']);
+    Route::get('/users/settings', [UserController::class, 'settingsUser'])->name('settingsUser');
 
-Route::get('/users/{user_name?}', [UserController::class, 'getUsers'])->name('users');
-Route::post('/users/update/{id}', [UserController::class, 'update']);
-Route::post('/users/create', [UserController::class, 'register']);
-Route::get('/users/delete/{id}', [UserController::class, 'delete']);
+    Route::get('/sessions/{user_name?}/{session_status?}', [UserController::class, 'getSessions'])->name('sessions');
 
-Route::get('/users/settings', [UserController::class, 'settingsUser'])->name('settingsUser');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/sendEmail', [MailController::class, 'sendMail'])->name('sendMail');
+});
 
-Route::get('/sessions/{user_name?}/{session_status?}', [UserController::class, 'getSessions'])->name('sessions');
-
-Route::get('/login', [AuthController::class, 'index'] )->name('login');
+Route::get('/login', function(){
+    return view('main');
+})->name('login');
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-
-Route::get('/sendEmail', [MailController::class, 'sendMail'])->name('sendMail');
